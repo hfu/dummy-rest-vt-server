1b9fdfa8c96797056e787a838cca8582f06eee80
Hidenori Fujimura <hfu@users.noreply.github.com>
2018-04-20 16:38:22 +0000

    update

--- server.js
+++ server.js
@@ -3,17 +3,30 @@ const express = require('express')
 const fs = require('fs')
 
 const app = express()
+const style = fs.readFileSync('style.json')
 
 app.get(
   '/arcgis/rest/services/Hosted/openmaptiles/VectorTileServer', 
   (req, res, next) => {
-  res.send({hello: 'world'})
+  res.set('Content-Type', 'application/json').send({
+    currentVersion: 10.4,
+    name: 'openmaptiles',
+    capabilities: 'TilesOnly',
+    type: 'vector',
+    defaultStyles: 'resources/styles',
+    tiles: ['https://hfujimura.gitlab.io/caf1801/{z}/{x}/{y}.mvt'],
+    exportTilesAllowed: true,
+    maxExportTilesCount: 10000,
+    resourceInfo: {
+      styleVersion: 8
+    }
+  })
 })
 
 app.get(
   '/arcgis/rest/services/Hosted/openmaptiles/VectorTileServer/resources/styles/root.json',
   (req, res, next) => {
-  res.send({hello: 'world'})
+  res.set('Content-Type', 'application/json').send(style)
 })
 
 spdy.createServer({
